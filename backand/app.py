de7307f223f8807f59b3818a9c56b41df2a181cb
Shreya pandey <pandeyshreya0206@gmail.com>
2026-03-31 14:05:00 +0530

    added backend code

--- backand/app.py
+++ backand/app.py
@@ -1,1 +1,20 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+data = []
+
+@app.route('/data', methods=['POST'])
+def receive():
+    d = request.json
+    data.append(d)
+
+    if d['temp'] > 30:
+        print("⚠️ ALERT!")
+
+    return {"msg": "ok"}
+
+@app.route('/data', methods=['GET'])
+def send():
+    return jsonify(data)
+
+app.run(debug=True)
